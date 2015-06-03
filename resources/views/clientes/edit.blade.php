ceacd564da7481fa0d0c4f20179775bac443e25c
ferraa <ferraa@hotmail.com>
2015-06-03 00:11:51 -0300

    Agregamos las vistas de Cliente

--- resources/views/clientes/edit.blade.php
+++ resources/views/clientes/edit.blade.php
@@ -13,20 +13,20 @@
                     <div class="form-group">
                         <label for="nombre" class="col-lg-2 control-label">Nombre:</label>
                         <div class="col-lg-10">
-                            <p>{{$cliente->full_name}}</p>
+                            <input class="form-control" placeholder="Nombre" name="nombre" type="text" value="{{$cliente->full_name}}" id="nombre">
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="pagina_web" class="col-lg-2 control-label">Pagina Web:</label>
                         <div class="col-lg-10">
-                            <p>{{$cliente->pagina_web}}</p>
+                            <input class="form-control" placeholder="Pagina_web" name="pagina_web" type="text" value="{{$cliente->pagina_web}}" id="pagina_web">
                         </div>
                     </div>
 
                     <div class="form-group">
                         <label for="telefono" class="col-lg-2 control-label">Telefono:</label>
                         <div class="col-lg-10">
-                            <p>{{$cliente->telefono}}</p>
+                            <input class="form-control" placeholder="Telefono" name="telefono" type="text" value="{{$cliente->telefono}}" id="telefono">
                         </div>
                     </div>
 
@@ -62,22 +62,28 @@
                                 <th>Celular</th>
                                 <th>Notas</th>
                                 <th>Foto</th>
+                                <th>Acciones</th>
                             </tr>
                             </thead>
                             <tbody>
                             @foreach($cliente->contactos as $contacto)
                             <tr>
-                                <td>{{$contacto->full_name}}</td>
-                                <td>{{$contacto->dni}}</td>
-                                <td>{{$contacto->mail}}</td>
-                                <td>{{$contacto->mail2}}</td>
-                                <td>{{$contacto->tel_particular}}</td>
-                                <td>{{$contacto->tel_laboral}}</td>
-                                <td>{{$contacto->extension}}</td>
-                                <td>{{$contacto->celular}}</td>
-                                <td>{{$contacto->notas}}</td>
+                                <td contenteditable>{{$contacto->full_name}}</td>
+                                <td contenteditable>{{$contacto->dni}}</td>
+                                <td contenteditable>{{$contacto->mail}}</td>
+                                <td contenteditable>{{$contacto->mail2}}</td>
+                                <td contenteditable>{{$contacto->tel_particular}}</td>
+                                <td contenteditable>{{$contacto->tel_laboral}}</td>
+                                <td contenteditable>{{$contacto->extension}}</td>
+                                <td contenteditable>{{$contacto->celular}}</td>
+                                <td contenteditable>{{$contacto->notas}}</td>
                                 <td> {!! $contacto->foto->render(35,35)!!}</td>
+                                <td>
 
+                                    <a href="{{ url('/clientes/'.$cliente->id_cliente.'/delete') }}" class="btn btn-danger btn-sm btn-delete">
+                                        <span class="glyphicon glyphicon-trash"></span></a>
+
+                                </td>
                             </tr>
                             @endforeach
 
@@ -97,22 +103,26 @@
                                 <th>Ciudad</th>
                                 <th>Provincia</th>
                                 <th>Pais</th>
-
+                                <th></th>
                             </tr>
                             </thead>
                             <tbody>
 
                             @foreach($cliente->domicilios as $domicilio)
                             <tr>
-                                <td><span id="calle" >{{$domicilio->calle}}</span></td>
-                                <td><span id="numero" >{{$domicilio->numero}}</td>
-                                <td><span id="piso" >{{$domicilio->piso}}</td>
-                                <td><span id="dpto" >{{$domicilio->depto}}</td>
-                                <td><span id="ciudad" >{{$domicilio->id_ciudad}}</td>
-                                <td><span id="provincia" ></td>
-                                <td><span id="pais" ></td>
+                                <td contenteditable>{{$domicilio->calle}}</td>
+                                <td contenteditable>{{$domicilio->numero}}</td>
+                                <td contenteditable>{{$domicilio->piso}}</td>
+                                <td contenteditable>{{$domicilio->depto}}</td>
+                                <td contenteditable>{{$domicilio->id_ciudad}}</td>
+                                <td contenteditable></td>
+                                <td contenteditable></td>
 
+                                <td>
 
+                                    <a href="#" class="btn btn-danger btn-sm btn-delete"><span class="glyphicon glyphicon-trash"></span></a>
+
+                                </td>
                             </tr>
                             @endforeach
 
@@ -127,16 +137,20 @@
                                 <th>Categoria</th>
                                 <th>Descripción</th>
                                 <th>Cuit/Cuil</th>
-
+                                <th></th>
                             </tr>
                             </thead>
                             <tbody>
                             @foreach($cliente->razonesSociales as $razonSocial)
                             <tr data-id="{{$razonSocial->id_razon_social}}">
-                                <td >{{$razonSocial->categoriaRazonSocial->descripcion}}</td>
-                                <td >{{$razonSocial->descripcion}}</td>
-                                <td>{{$razonSocial->cuit_cuil}}</td>
+                                <td contenteditable>{{$razonSocial->categoriaRazonSocial->descripcion}}</td>
+                                <td contenteditable>{{$razonSocial->descripcion}}</td>
+                                <td contenteditable>{{$razonSocial->cuit_cuil}}</td>
+                                <td>
 
+                                    <a href="#" class="btn btn-danger btn-sm btn-delete"><span class="glyphicon glyphicon-trash"></span></a>
+
+                                </td>
                             </tr>
                             @endforeach
 
@@ -154,14 +168,17 @@
 
                         <div class="form-group">
                             <div class="col-lg-offset-2 col-lg-10">
-                                <a href="{{ url('/clientes') }}" class="btn btn-success btn-sm">Volver</a>
-
+                                <button type="submit" class="btn btn-primary">Guardar</button>
 
                             </div>
                         </div>
 
                     </form>
 
+                {!! Form::open(array('method' => 'DELETE', 'route'=>['clientes.destroy',$cliente->id_cliente], 'id'=>'form-delete')) !!}
+
+                {!! Form::submit('Borrar', array('class' => 'btn btn-danger btn-sm')) !!}
+                {!! Form::close()!!}
 
                     </div>
                     </div>
